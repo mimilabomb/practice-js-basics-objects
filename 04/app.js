d33500deca0e31b559c1c0257740fa74ed57cf0f
mimilabomb <bartoszsz13@gmail.com>
2024-06-05 19:14:50 +0200

    finish 04

--- 04/app.js
+++ 04/app.js
@@ -5,5 +5,19 @@ const user = {
         day: '14',
         month: '04',
         year: '1985'
+        // day: '5',
+        // month: '6',
+        // year: '1985'
     }
+}
+
+const date= new Date();
+const currentDay = date.getDate();
+const currentMonth = date.getMonth()+1;
+
+
+if (parseInt(user.born.day) === currentDay && parseInt(user.born.month) === currentMonth){
+    console.log(user.firstName, user.lastName, 'ma dziś urodziny!!');
+}else{
+    console.log(user.firstName, user.lastName, 'nie ma dziś urodzin');
 }
